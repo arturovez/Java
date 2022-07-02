2e7cf30ad26124ce36f84a70eb5ffb35b7a7a8cf
arturovez <arturo.vez@gmail.com>
2022-07-02 16:16:00 -0500

    art1

--- EJERCICIOS/Promedio.java
+++ EJERCICIOS/Promedio.java
@@ -1,14 +1,8 @@
 import java.util.Scanner;
-
 public class Promedio {
     public static void main(String [] args){
-        String nombreSTR;
-        double promedio;
-        int i = 0;
         
-
-
-
     }
-}
 
+
+}

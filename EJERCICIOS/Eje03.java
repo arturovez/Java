6894635c5019b004784e0b7473d07c55f0123bb3
arturovez <arturo.vez@gmail.com>
2022-07-02 17:17:54 -0500

    aVez87

---
+++ EJERCICIOS/Eje03.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Eje03 {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        int mes = 0;
+        int bis = 0;
+
+    }
+}
--- EJERCICIOS/Promedio.java
+++ EJERCICIOS/Promedio.java
@@ -1,8 +1,30 @@
 import java.util.Scanner;
-public class Promedio {
-    public static void main(String [] args){
-        
-    }
-
+public class Promedio{
+    public static void main(String [] args) {
+        String nombre;
+        double calificacion, suma = 0, promedio = 0;
+        int i = 1;
+        Scanner in = new Scanner (System.in);
+        System.out.println("Nombre del alumno");
+                nombre= in.nextLine();
+        while(i<6){
+                            System.out.println("Ingresa la calificacion" + i + "de" + nombre );
+calificacion=in.nextInt();
+            if((calificacion<11)&&(calificacion>0)){
+                i=i+1;
+                suma=suma+calificacion;
+                               promedio = suma/i; 
+}
 
 }
+
+
+if(promedio>6){
+    System.out.println("El promedio de " + nombre + " es de " + promedio + " Felicidades Aprobaste");
+}
+else{
+    System.out.println("El promedio de " + nombre + " es de " + promedio + " Reprobaste, echale ganas");
+}
+    }
+}
+
